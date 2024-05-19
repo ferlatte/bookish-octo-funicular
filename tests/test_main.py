07b8e9c0f6bcf155b299c08e4a82b6293caff2ce
Mark Ferlatte <mark@ferlatte.net>
2024-05-19 16:38:20 -0700

    Move tests into tests dir.

--- tests/test_main.py
+++ tests/test_main.py
@@ -27,14 +27,14 @@ class TestMain(unittest.TestCase):
         self.assertGreaterEqual(len(events), 1)
 
     def test_calendarFromICSFile(self) -> None:
-        cal = main.calendarFromICSFile("test_cal1.ics")
+        cal = main.calendarFromICSFile("tests/test_cal1.ics")
         self.assertIsInstance(cal, Calendar)
         events = cal.walk("vevent")
         self.assertEqual(len(events), 1)
 
     def test_mergeEventsFromCalendars(self) -> None:
-        cal1 = main.calendarFromICSFile("test_cal1.ics")
-        cal2 = main.calendarFromICSFile("test_cal2.ics")
+        cal1 = main.calendarFromICSFile("tests/test_cal1.ics")
+        cal2 = main.calendarFromICSFile("tests/test_cal2.ics")
         cal = main.mergeEventsFromCalendars([cal1, cal2])
         events = cal.walk("vevent")
         self.assertEqual(len(events), 2)
